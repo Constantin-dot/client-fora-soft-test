c7afb41eb2fa88cf9e56e58a3012ca2ca451e364
Constantin <konstantinkspb@gmail.com>
2021-06-11 00:18:07 +0300

    fixed path of gh pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ const App = React.memo(() => {
     return (
         <div className={styles.App}>
             <Switch>
-                <Route path={"/"} exact render={() => <InitialForm />} />
+                <Route
+                    path={"/client-fora-soft-test"}
+                    exact
+                    render={() => <InitialForm />}
+                />
                 <Route path={"/chat-room"} render={() => <ChatRoom />} />
                 <Route path={"*"} render={() => <div>404 NOT FOUND</div>} />
             </Switch>
